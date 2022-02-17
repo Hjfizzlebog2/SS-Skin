cbf0a75186e3a44ee693d3f100191bd9c3c7db73
Brian Molina <brianscott.molina@calbaptist.edu>
2022-02-16 16:43:17 -0800

    registered home page code

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,71 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const RegisteredHomePage(),
+    );
+  }
+}
+
+// class for the registered home page screen
+class RegisteredHomePage extends StatelessWidget {
+  const RegisteredHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Skin Safety Scanner"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column (
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              "Welcome Back!",
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w500,
+                fontSize: 50),
+            ),
+          ),
+          Container(
+            child: Image.asset('assets/homepagepic.jpg')
+          ),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            color: Colors.cyan,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                  Icons.add
+              ),
+              label: const Text("Create new entry"),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey)
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            color: Colors.cyan,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                  Icons.add
+              ),
+              label: const Text("View log history"),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey)
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
