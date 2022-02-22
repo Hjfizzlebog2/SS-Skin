07247fc5c68b5ef0deec79e133b965efdd94f6db
saidbrandonsaid <brandonmatthew.said@calbaptist.edu>
2022-02-21 21:53:21 -0800

    Rearranged containers on ResetPassword and edited button links.

--- lib/ResetPassword.dart
+++ lib/ResetPassword.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:skin_safety_scanner/RegisteredHomePage.dart';
 import 'ScreeningBreakdown.dart';
 import 'LogHistory.dart';
 
-// class for the registered home page screen
+// class for the reset password screen
 class RegisteredHomePage extends StatelessWidget {
   const RegisteredHomePage({Key? key}) : super(key: key);
 
+  // TODO: Figure out how to emulate this page
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,10 +16,33 @@ class RegisteredHomePage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.redAccent,
         ),
-        body: Column (
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(15),
+              width: 270,
+              height: 90,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RegisteredHomePage()),
+                  );
+                },
+                icon: const Icon(
+                  Icons.arrow_back,
+                ),
+                style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.blueGrey)),
+                label: const Text("Back",
+                    style:
+                    TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              ),
+            ),
             Container(
               alignment: Alignment.topCenter,
               padding: const EdgeInsets.all(10),
@@ -29,55 +54,22 @@ class RegisteredHomePage extends StatelessWidget {
                     fontSize: 34),
               ),
             ),
-            Image.asset('images/registeredhomepage.jpg'),
             Container(
               padding: const EdgeInsets.all(15),
               width: 270,
               height: 90,
               child: ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ScreeningBreakdown()),
-                  );
-                },
-                icon: const Icon(
-                    Icons.add
-                ),
-                label: const Text("Create new entry",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18)),
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text("Reset Password",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
                 style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey)
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              width: 270,
-              height: 90,
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LogHistory()),
-                  );
-                },
-                icon: const Icon(
-                    Icons.add
-                ),
-                label: const Text("View log history",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18)),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey)
-                ),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blueGrey)),
               ),
             ),
           ],
-        )
-    );
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:skin_safety_scanner/RegisteredHomePage.dart';
+import 'ResetPassword.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       //FIXME: Change back the below to RegisteredHomePage()
-      home: RegisteredHomePage(),
-    );
+      home: ResetPassword(),
+    ); // Material App
   }
 }
