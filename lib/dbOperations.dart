7ae6baf2f0a728cd8d2baaaf498823dead15c2aa
Hjfizzlebog2 <ethandm54@gmail.com>
2022-02-23 13:45:58 -0800

    CreateAccount page

--- lib/dbOperations.dart
+++ lib/dbOperations.dart
@@ -1,27 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 FirebaseFirestore fireStore = FirebaseFirestore.instance;
+CollectionReference users = fireStore.collection('User');
 
-Future<void> uploadingData(String fName, String lName,
-    String email, String password) async {
-  await FirebaseFirestore.instance.collection("User").add({
-    'Fname': fName,
-    'Lname': lName,
-    'Email': email,
-    'Password': password,
-  });
+Future<void> uploadingData(
+    String fName, String lName, String email, String password) async {
+  return users
+      .add({
+        'Fname': fName,
+        'Lname': lName,
+        'Email': email,
+        'Password': password,
+      })
+      .then((value) => print("User Added"))
+      .catchError((error) => print("Failed to add user: $error"));
 }
 
-Future<void> editProduct(bool _isFavourite,String id) async {
-  FirebaseFirestore.instance
-      .collection("User");
-      //.document(id)
-      //.updateData({"isFavourite": !_isFavourite});
+Future<void> editProduct(bool _isFavourite, String id) async {
+  FirebaseFirestore.instance.collection("User");
+  //.document(id)
+  //.updateData({"isFavourite": !_isFavourite});
 }
 
 Future<void> deleteProduct(DocumentSnapshot doc) async {
-  FirebaseFirestore.instance
-      .collection("User");
-      //.document(doc.documentID)
-      //.delete();
+  FirebaseFirestore.instance.collection("User");
+  //.document(doc.documentID)
+  //.delete();
 }
