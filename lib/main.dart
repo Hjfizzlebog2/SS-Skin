d9d90a65a9d23848223a1cd44939e16f5ebeded8
saidbrandonsaid <brandonmatthew.said@calbaptist.edu>
2022-03-28 12:46:58 -0700

    Created NextStepsMelanoma, GeneratedReport, and FurtherDetails. Working through NearbyDermatologists still, but it's doing better.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+
 import 'package:flutter/material.dart';
 import 'package:ss_skin_project/LoginScreen.dart';
+import 'package:ss_skin_project/NearbyDermatologists.dart'; //Remove me
+
 
 // main class, runs app
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { //Might need to make this stateful widget
   const MyApp({Key? key}) : super(key: key);
 
   @override
